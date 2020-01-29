0ef7d03efc9199bbbc348a38d7bf754b8cad7afe
sjnyos <wrksuraj@gmail.com>
2020-01-29 22:56:48 +0545

    Added Trip, Reservation, Users,tripCategories models

--- app/src/main/java/com/machamasisuraj/socialapp/Model/User.java
+++ app/src/main/java/com/machamasisuraj/socialapp/Model/User.java
@@ -1,35 +1,23 @@
 package com.machamasisuraj.socialapp.Model;
 
+import java.util.Date;
+
 public class User {
 
-    private String firstName;
-    private String lastName;
-    private String username;
-    private String password;
-    private String image;
+    private String username, password,firstname,lastname,
+    bio,interestedin,gender,userstatus;
+    private Date dateofbirth;
 
-    public User(String firstName, String lastName, String username, String password, String image) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public User(String username, String password, String firstname, String lastname, String bio, String interestedin, String gender, String userstatus, Date dateofbirth) {
         this.username = username;
         this.password = password;
-        this.image = image;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.bio = bio;
+        this.interestedin = interestedin;
+        this.gender = gender;
+        this.userstatus = userstatus;
+        this.dateofbirth = dateofbirth;
     }
 
     public String getUsername() {
@@ -48,11 +36,59 @@ public class User {
         this.password = password;
     }
 
-    public String getImage() {
-        return image;
+    public String getFirstname() {
+        return firstname;
     }
 
-    public void setImage(String image) {
-        this.image = image;
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getInterestedin() {
+        return interestedin;
+    }
+
+    public void setInterestedin(String interestedin) {
+        this.interestedin = interestedin;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getUserstatus() {
+        return userstatus;
+    }
+
+    public void setUserstatus(String userstatus) {
+        this.userstatus = userstatus;
+    }
+
+    public Date getDateofbirth() {
+        return dateofbirth;
+    }
+
+    public void setDateofbirth(Date dateofbirth) {
+        this.dateofbirth = dateofbirth;
     }
 }
