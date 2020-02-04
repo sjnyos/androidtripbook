309a87730515238fcef990e2bf66dc040afb5497
sjnyos <wrksuraj@gmail.com>
2020-02-04 23:15:29 +0545

    Banner Activity to Fragment

--- app/src/main/java/com/machamasisuraj/socialapp/GUI/BannerActivity.java
+++ app/src/main/java/com/machamasisuraj/socialapp/GUI/BannerActivity.java
@@ -1,9 +1,12 @@
 package com.machamasisuraj.socialapp.GUI;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -11,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.machamasisuraj.socialapp.BLL.BannerBll;
 import com.machamasisuraj.socialapp.BLL.ImageUpload;
@@ -19,21 +23,28 @@ import com.machamasisuraj.socialapp.R;
 import com.machamasisuraj.socialapp.permissonsAndUri.LinkPermisson;
 import com.machamasisuraj.socialapp.permissonsAndUri.StoragePath;
 
-public class BannerActivity extends AppCompatActivity {
+import static android.app.Activity.RESULT_OK;
+
+public class BannerActivity extends Fragment {
+    private Context mContext;
+
+    public BannerActivity( Context mContext) {
+        this.mContext = mContext;
+    }
 
     private ImageView bannerImage;
     private EditText bannerName,desc;
     private Button upload,save;
     private String imagepath="";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_banner);
-        bannerImage=findViewById(R.id.imageView2banner);
-        bannerName=findViewById(R.id.editTextbanner);
-        desc=findViewById(R.id.editTextdesc);
-        upload=findViewById(R.id.uploadbutton2);
-        save=findViewById(R.id.buttonsave);
+      View view  = inflater.inflate(R.layout.activity_banner,container,false);
+        bannerImage= view.findViewById(R.id.imageView2banner);
+        bannerName=view.findViewById(R.id.editTextbanner);
+        desc=view.findViewById(R.id.editTextdesc);
+        upload=view.findViewById(R.id.uploadbutton2);
+        save=view.findViewById(R.id.buttonsave);
 
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,7 +53,7 @@ public class BannerActivity extends AppCompatActivity {
                     BrowseImage();
                 } catch (Exception e) {
                     e.printStackTrace();
-                    Toast.makeText(BannerActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(mContext, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -53,13 +64,14 @@ public class BannerActivity extends AppCompatActivity {
                 UploadImageAndSaveData();
             }
         });
+        return view;
 
 
     }
 
     private void BrowseImage() throws Exception {
         // checkPermissionForReadExtertalStorage();
-        LinkPermisson linkPermisson= new LinkPermisson(this);
+        LinkPermisson linkPermisson= new LinkPermisson(mContext);
         linkPermisson.requestPermissionForReadExtertalStorage();
 
         Intent intent = new Intent(Intent.ACTION_PICK);
@@ -68,22 +80,22 @@ public class BannerActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (resultCode == RESULT_OK) {
             if (data == null) {
-                Toast.makeText(this, "Please select an image ", Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, "Please select an image ", Toast.LENGTH_SHORT).show();
             }
         }
         Uri uri = data.getData();
         bannerImage.setImageURI(uri);
-        StoragePath storagePath= new StoragePath(this);
+        StoragePath storagePath= new StoragePath(mContext);
         imagepath = storagePath.getRealPathFromUri(uri);
     }
 
     private void UploadImageAndSaveData(){
-        ImageUpload imageUpload = new ImageUpload(imagepath,this);
+        ImageUpload imageUpload = new ImageUpload(imagepath,mContext);
         String getmageName= imageUpload.uploadFile();
         if(!getmageName.equals("Error")){
             SaveData(getmageName);
@@ -94,7 +106,7 @@ public class BannerActivity extends AppCompatActivity {
         BannerItem bannerItem= new BannerItem(bannerName.getText().toString(),setimageName,desc.getText().toString());
         BannerBll bannerBll= new BannerBll();
         if(bannerBll.InsertBanner(bannerItem)){
-            Toast.makeText(this, "Data Inserted Successfuly", Toast.LENGTH_SHORT).show();
+            Toast.makeText(mContext, "Data Inserted Successfuly", Toast.LENGTH_SHORT).show();
         }
 
     }
