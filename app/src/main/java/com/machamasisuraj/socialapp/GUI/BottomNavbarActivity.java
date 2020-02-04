43b6b53eb55b58e3fc00e1a3e5224b890a3c10be
sjnyos <wrksuraj@gmail.com>
2020-02-04 23:37:30 +0545

    Drawerlayout to BottomActivity

--- app/src/main/java/com/machamasisuraj/socialapp/GUI/BottomNavbarActivity.java
+++ app/src/main/java/com/machamasisuraj/socialapp/GUI/BottomNavbarActivity.java
@@ -1,19 +1,31 @@
 package com.machamasisuraj.socialapp.GUI;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
 import com.machamasisuraj.socialapp.R;
 
 public class BottomNavbarActivity extends AppCompatActivity {
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navbar);
+
+
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         BannerActivity firstFragment = new BannerActivity(this);
@@ -21,6 +33,34 @@ public class BottomNavbarActivity extends AppCompatActivity {
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
 
+        navigations();
 
+
+
+    }
+
+    public void navigations(){
+        dl = findViewById(R.id.bottomactivityDrawerlayout);
+        t = new ActionBarDrawerToggle(this, dl,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        t.setDrawerIndicatorEnabled(true);
+        dl.addDrawerListener(t);
+        t.syncState();
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+        nv = findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+                dl.closeDrawer(GravityCompat.START);
+                if(item.getItemId()==R.id.navigation_aboutus){
+
+                }
+                return false;
+
+            }
+        });
     }
 }
