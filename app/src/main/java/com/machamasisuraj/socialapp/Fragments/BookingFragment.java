64de6d7fbe879e409f936386e6c626ca63e037d9
sjnyos <wrksuraj@gmail.com>
2020-02-15 05:42:58 +0545

    Added BookingFragment to view users Bookings

---
+++ app/src/main/java/com/machamasisuraj/socialapp/Fragments/BookingFragment.java
@@ -0,0 +1,35 @@
+package com.machamasisuraj.socialapp.Fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.machamasisuraj.socialapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BookingFragment extends Fragment {
+
+    private Context mContext;
+    private RecyclerView booking_Recycler;
+    public BookingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_booking, container, false);
+    }
+
+}
