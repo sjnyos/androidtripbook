c7d404972fc37bca558e6db1f512d91e8c652e7a
sjnyos <wrksuraj@gmail.com>
2020-02-15 16:46:07 +0545

    Added Notificatin Manager to the App

---
+++ app/src/main/java/com/machamasisuraj/socialapp/Utilities/CreateChannel/CreateChannel.java
@@ -0,0 +1,36 @@
+package com.machamasisuraj.socialapp.Utilities.CreateChannel;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+
+public class CreateChannel {
+    Context context;
+    public final  static String Channel_1="channel 1";
+    public final static  String Channel_2="channel 2";
+
+    public CreateChannel(Context context) {
+        this.context = context;
+    }
+
+    public void createChannel(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel1 = new NotificationChannel(
+                    Channel_1,
+                    "CreateChannel Name 1",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel1.setDescription(("this is CreateChannel 1"));
+            NotificationChannel channel2 = new NotificationChannel(
+                    Channel_2,
+                    "CreateChannel Name 2",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            channel2.setDescription("this is channel 2");
+            NotificationManager manager= context.getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+            manager.createNotificationChannel(channel2);
+        }
+    }
+}
--- app/src/main/java/com/machamasisuraj/socialapp/Utilities/NotificationViewer.java
+++ app/src/main/java/com/machamasisuraj/socialapp/Utilities/NotificationViewer.java
@@ -1,35 +1,56 @@
 package com.machamasisuraj.socialapp.Utilities;
 
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.machamasisuraj.socialapp.GUI.MainActivity;
 import com.machamasisuraj.socialapp.R;
+import com.machamasisuraj.socialapp.Utilities.CreateChannel.CreateChannel;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
 
+
 public class NotificationViewer {
+    private NotificationManagerCompat notificationManagerCompat;
     private Context mContext;
+    CreateChannel createChannel;
+
     public NotificationViewer(Context mContext){
         this.mContext=mContext;
 
+        notificationManagerCompat = NotificationManagerCompat.from(mContext);
+        createChannel = new CreateChannel(mContext);
+        createChannel.createChannel();
+
     }
 
-    NotificationCompat.Builder builder =
-            new NotificationCompat.Builder(mContext)
-                    .setSmallIcon(R.drawable.abc)
-                    .setContentTitle("Notifications Example")
-                    .setContentText("This is a test notification");
 
-    Intent notificationIntent = new Intent(mContext, MainActivity.class);
-    PendingIntent contentIntent = PendingIntent.getActivity(mContext, 0, notificationIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT);
-      builder.setContentIntent(contentIntent);
+    private void Dispplaynotification2() {
+        Notification notification = new NotificationCompat.Builder(mContext,CreateChannel.Channel_2)
+                .setSmallIcon(R.drawable.ic_book_black_24dp)
+                .setContentTitle("Android Message hehehe")
+                .setContentText("this is the test messagte for android message")
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        notificationManagerCompat.notify(1,notification);
 
-    // Add as notification
-    NotificationViewer manager = (NotificationViewer) mContext.getSystemService(NOTIFICATION_SERVICE);
-      manager.notify(0, builder.build());
+
+    }
+
+    private void DisplayNotification() {
+
+        Notification notification = new NotificationCompat.Builder(mContext,CreateChannel.Channel_1)
+                .setSmallIcon(R.drawable.ic_book_black_24dp)
+                .setContentTitle("Black box")
+                .setContentText("this is the black box message")
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        notificationManagerCompat.notify(2,notification);
+
+    }
 }
