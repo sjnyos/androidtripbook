317e9c507a0bd10dcc037c42bc72eb77a1b3cd6b
sjnyos <wrksuraj@gmail.com>
2020-02-15 12:57:44 +0545

    fixing some unwanted codes

--- app/src/main/java/com/machamasisuraj/socialapp/GUI/MapsActivity.java
+++ app/src/main/java/com/machamasisuraj/socialapp/GUI/MapsActivity.java
@@ -51,7 +51,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onSuccess(Location location) {
                 if (location != null) {
                     currentLocation = location;
-                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude() + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(getApplicationContext(), currentLocation.getLatitude() + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
                     SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
                     assert supportMapFragment != null;
                     supportMapFragment.getMapAsync(MapsActivity.this);
