c2686dc3c6963ef54ab6d95423811cc78e77879c
sjnyos <wrksuraj@gmail.com>
2020-02-15 13:48:36 +0545

    proximity Sensor Added on BottomNavBar Activity

--- app/src/main/java/com/machamasisuraj/socialapp/GUI/BottomNavbarActivity.java
+++ app/src/main/java/com/machamasisuraj/socialapp/GUI/BottomNavbarActivity.java
@@ -10,9 +10,15 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,14 +48,13 @@ public class BottomNavbarActivity extends AppCompatActivity {
         bottomnavigation.setSelectedItemId(R.id.homego);
 
 
-
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         TripListFragment firstFragment = new TripListFragment(this);
         fragmentTransaction.replace(R.id.frame_container, firstFragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
-
+        ProximitySensor();
         navigations();
         BottomNavitaionMenus();
         PopulatatChatHead();
@@ -133,4 +138,48 @@ public class BottomNavbarActivity extends AppCompatActivity {
 
 
     }
+
+    public void ProximitySensor() {
+        SensorManager mySensorManager;
+        Sensor myProximitySensor;
+        mySensorManager = (SensorManager) getSystemService(
+                Context.SENSOR_SERVICE);
+        myProximitySensor = mySensorManager.getDefaultSensor(
+                Sensor.TYPE_PROXIMITY);
+        if (myProximitySensor == null) {
+            Toast.makeText(this, "No Proximity Sensor!", Toast.LENGTH_SHORT).show();
+        } else {
+            mySensorManager.registerListener(proximitySensorEventListener,
+                    myProximitySensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+
+    }
+    SensorEventListener proximitySensorEventListener
+            = new SensorEventListener() {
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // TODO Auto-generated method stub
+        }
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            // TODO Auto-generated method stub
+            WindowManager.LayoutParams params = BottomNavbarActivity.this.getWindow().getAttributes();
+            if(event.sensor.getType()==Sensor.TYPE_PROXIMITY){
+
+                if(event.values[0]==0){
+                    params.flags |= WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
+                    params.screenBrightness = 0;
+                    getWindow().setAttributes(params);
+                }
+                else{
+                    params.flags |= WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
+                    params.screenBrightness = -1f;
+                    getWindow().setAttributes(params);
+                }
+            }
+        }
+    };
 }
+
